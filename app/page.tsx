c54ca2c4a77a87092d70e8679f09aa2befa4033d
ashishkudu <kuduashish9647@gmail.com>
2026-08-14 15:54:01 -0400

    feat: enable project image lightbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import PortfolioShell from "@/components/PortfolioShell";
 import RefreshToHome from "@/components/RefreshToHome";
+import ImageClickHandler from "@/components/ImageClickHandler";
 
 export default function Home() {
   return (
     <>
       <RefreshToHome />
+      <ImageClickHandler />
       <PortfolioShell />
     </>
   );
