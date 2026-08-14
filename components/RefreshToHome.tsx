9025675411d284bcae8283f2cc866215cb9d42c6
ashishkudu <kuduashish9647@gmail.com>
2026-08-14 11:56:33 -0400

    Add subtle home landing bounce on refresh - v1.3.8

--- components/RefreshToHome.tsx
+++ components/RefreshToHome.tsx
@@ -7,8 +7,21 @@ export default function RefreshToHome() {
     if (window.location.hash) {
       window.history.replaceState(null, "", window.location.pathname + window.location.search);
     }
+
     window.scrollTo(0, 0);
-    requestAnimationFrame(() => window.scrollTo(0, 0));
+
+    const frame = requestAnimationFrame(() => {
+      window.scrollTo(0, 0);
+      document.documentElement.classList.add("home-refresh-bounce");
+      window.setTimeout(() => {
+        document.documentElement.classList.remove("home-refresh-bounce");
+      }, 650);
+    });
+
+    return () => {
+      cancelAnimationFrame(frame);
+      document.documentElement.classList.remove("home-refresh-bounce");
+    };
   }, []);
 
   return null;
