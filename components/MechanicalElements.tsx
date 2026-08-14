a05c1501710409489e4e78bf2c50e0928a46d43a
ashishkudu <kuduashish9647@gmail.com>
2026-08-14 11:41:07 -0400

    Add AUV hero image beside title - v1.2.8

--- components/MechanicalElements.tsx
+++ components/MechanicalElements.tsx
@@ -1,3 +1,11 @@
 export default function MechanicalElements() {
-  return null;
+  return (
+    <div className="mechanical-elements" aria-hidden="true">
+      <img
+        src="/projects/auv/AUV%20Model.png"
+        alt=""
+        draggable={false}
+      />
+    </div>
+  );
 }
